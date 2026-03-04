9b2d5d7f257538d9cdce2341b8441d5189d99cb3
vinaynaidu-8 <vinaynaidupinninti@gmail.com>
2026-03-04 20:21:46 +0530

    Update run_pipeline.sh

--- pipeline/run_pipeline.sh
+++ pipeline/run_pipeline.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Running pipeline..."
+
+echo "Starting Cloud Governance Pipeline..."
+
 python3 pipeline/resource_discovery.py
+python3 pipeline/metrics_collection.py
 python3 pipeline/cost_collection.py
 python3 pipeline/optimization_engine.py
-echo "Pipeline finished."
+
+echo "Pipeline finished successfully."
